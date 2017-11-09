85972f5a09c623bcf7cf3f893fadf34ae02ac064
lishenggen <lishenggen@qjdchina.com>
2017-11-09 20:13:58 +0800

    dev report

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-
-export const PAGE_SIZE = 10;
-export const PREFIX = 'crm/pages';
--- src/services/users.js
+++
@@ -1,6 +0,0 @@
-import request from '../utils/request';
-import { PAGE_SIZE } from '../constants';
-
-export function fetch({ page }) {
-  return request(`https://cnodejs.org/api/v1/topics?page=${page}&limit=${PAGE_SIZE}`);
-}
